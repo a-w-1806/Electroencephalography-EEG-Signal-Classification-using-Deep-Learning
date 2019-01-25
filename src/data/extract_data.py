d04fce9e45d47417fcaec354b38d319c5db4c79a
Yuchen Wang <yuchenwang.sh@gmail.com>
2019-01-25 17:10:31 +0800

    extract_data with docs.

--- src/data/extract_data.py
+++ src/data/extract_data.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 def read_BCIIII_p300_mat(file_path):
+    """ Read .mat file into a dict """
     data = loadmat(file_path)
     del data['__globals__'], data['__header__'], data['__version__']
     return data
@@ -34,27 +35,55 @@ def find_timepoints_1D(single_stimulus_code):
 
 
 def extract_data(file_path, seconds_to_slice):
+    """ Given a file_path, return the data in a format which is easy to use.
+    
+    Args:
+        file_path (string): The path of the given .mat file.
+        seconds_to_slice (int): The number of seconds you want to slice as your data point after an intensify happens.
+
+    Return:
+        dict: 
+            if is_training:
+                {
+                    'signal': [num_characters, num_times, num_intens, num_electrodes, points_per_intens]
+                    'code': [num_characters, num_times, num_intens], meaning as 'StimulusCode'
+                    'label': [num_characters, num_times, num_intens], meaning as 'StimulusType'
+                    'targetchar': [num_characters, num_times], meaning as 'TargetChar'
+                }
+            else:
+                {
+                    'signal'
+                    'code'
+                }
+    """
+
+    # if true then it's training set else test set
+    if 'StimulusType' in data.keys():
+        is_training = True
+    else:
+        is_training = False
+
     data = read_BCIIII_p300_mat(file_path)
     # Training:Signal,StimulusCode,StimulusType,TargetChar,Flashing
     # Test:Signal,StimulusCode,Flashing
     num_characters = data['Signal'].shape[0]
     num_timesteps = data['Signal'].shape[1]
     num_electrodes = data['Signal'].shape[2]
-    num_intens = 12
-    num_times = 15
+    num_intens = 12 # 6 rows + 6 cols
+    num_times = 15  # Each character is runned for 15 times, so 12 * 15 = 180 intensifies
     Fs = 240  # Sampling frequency 240Hz
     points_per_intens = int(Fs * seconds_to_slice)
 
     # The shape of return I want
     signal = np.zeros([num_characters, num_times, num_intens, num_electrodes, points_per_intens])
     code = np.zeros([num_characters, num_times, num_intens])
-    if 'StimulusType' in data.keys():  # if true then it's training set else test set
+    if is_training:  # if true then it's training set else test set
         label = np.zeros([num_characters, num_times, num_intens])
 
     for character in range(num_characters):
         # All electrodes start at the same time so pick 0 is fine
         timepoints = find_timepoints_1D(data['StimulusCode'][character, :])  # (12*15,)
-        timepoints = timepoints.reshape([-1, 12])  # (15,12)
+        timepoints = timepoints.reshape([-1, num_intens])  # (15,12)
         for time in range(num_times):
             for intens in range(num_intens):
                 start = timepoints[time, intens]
@@ -63,9 +92,9 @@ def extract_data(file_path, seconds_to_slice):
                 sliced_signal = sliced_signal.transpose([1, 0])  # (64,end-start)
                 signal[character, time, intens, :, :] = sliced_signal
                 code[character, time, intens] = data['StimulusCode'][character, start]
-                if 'StimulusType' in data.keys():
+                if is_training:
                     label[character, time, intens] = data['StimulusType'][character, start]
-    if 'StimulusType' in data.keys():
+    if is_training:
         return {'signal': signal, 'code': code, 'label': label, 'targetchar': data['TargetChar']}
     else:
         return {'signal': signal, 'code': code}
